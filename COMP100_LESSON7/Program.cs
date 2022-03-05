7e4cc8ebeb951e5473d19927b7dca39e4486f633
pzsbbfan <95864657+pzsbbfan@users.noreply.github.com>
2022-03-05 17:21:19 -0500

    Add project files.

---
+++ COMP100_LESSON7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP100_LESSON7
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int prompt = 0;
+            while (prompt != 3)
+            {
+
+            }
+            
+            
+            
+            
+            Console.WriteLine("++++++++++++++++++++++++");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
